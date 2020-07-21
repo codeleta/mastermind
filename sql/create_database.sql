3cc17e5a3576710e9fe1b194892bac927a64004c
yademidenko <admin@papara.ru>
2020-07-21 17:05:48 +0300

    Initial commit

---
+++ sql/create_database.sql
@@ -0,0 +1,8 @@
+/*
+This file is used to bootstrap development database.
+
+Note: ONLY development database;
+*/
+
+CREATE USER mastermind SUPERUSER;
+CREATE DATABASE mastermind OWNER mastermind ENCODING 'utf-8';
